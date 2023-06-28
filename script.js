46f1c5983b754877d63596f1cd0fde294baa198e
Genexi <Pashaz1994@gmail.com>
2023-06-28 12:43:31 +0300

    work2

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const modalWindow = document.querySelector('.modal-window');
+const overlay = document.querySelector('.overlay');
+const btnCloseModalWindow = document.querySelector('.close-modal-window');
+const btnsShowModalWindow = document.querySelectorAll('.show-modal-window');
+console.log(btnsShowModalWindow);
+
+const ShowModalWindow = function () {
+  console.log('Click!');
+  modalWindow.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModalWindow = function () {
+  modalWindow.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsShowModalWindow.length; i++) {
+  btnsShowModalWindow[i].addEventListener('click', ShowModalWindow);
+}
+
+btnCloseModalWindow.addEventListener('click', closeModalWindow);
+
+overlay.addEventListener('click', closeModalWindow);
